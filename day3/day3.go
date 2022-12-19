ecdc4df405beb118479ad932681ac5e7f04b2ae3
Jonathan Pichardo <jonathan.pichardo@classdojo.com>
2022-12-19 04:13:51 +0000

    Day 3 (partial)

---
+++ day3/day3.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	line := "vJrwpWtwJgWrhcsFMMfFFhFp"
+
+	firstCompartment := line[len(line)/2:]
+	secondCompartment := line[:len(line)/2]
+
+	var duplicate string
+
+	fmt.Println(duplicate)
+
+	for i := 0; i < len(firstCompartment); i++ {
+		idx := strings.IndexByte(secondCompartment, firstCompartment[i])
+		if idx > -1 {
+			duplicate = firstCompartment[i]
+		}
+	}
+
+	fmt.Println(firstCompartment, secondCompartment)
+
+	fmt.Println("a"[0] - 96)
+
+}
